d929320ce16f94f71199eedc4d9a157fe0185174
sparsh <130464884+sparshsharma81@users.noreply.github.com>
2025-11-13 14:36:00 +0530

    Dynamic programming

---
+++ Lecture16(No.of_distinct_subsequence)/Leetcode115.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Leetcode115{
+    List<List<String>>res = new ARray
+    public static void generate(String s, String ans, List<List<String>>ress){
+        if(ans.length() == 0){
+            // ress.add(new ArrayList<>(ans));
+        }
+    }
+    public static void main(String args[]){
+        //now we are generating the number of distinct subsequences 
+        //so we will try the brute force approach
+        //so basically we will generate all the subsequences of the string s
+         String s = "rabbbit";
+         String t = "rabbit";
+         List<List<String>>ans = new ArrayList<>();
+         //now in this arraylist..all the elements will get stored 
+        //  generate(s,"",ans,t);
+    }
+}
